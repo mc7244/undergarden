806b3172266436f3157161fcee5ebed8d076b010
Michele Beltrame <mb@italpro.net>
2018-09-26 12:58:34 +0200

    Us maplit macros to clean hashmap creation

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,37 @@
 mod unend;
 use unend::ConsoleIO;
-use std::collections::HashMap;
+
+#[macro_use] extern crate maplit;
 
 // Our games does I/O from the console, so we implement the relative trait
 // No need to actually implement methods, the default ones will do
 impl<T: unend::Visitable> ConsoleIO for unend::Game<T> {}
 
 fn main() {
-    let mut hallway_exits = HashMap::new();
-    hallway_exits.insert("n".to_string(), unend::Exit::Visitable("kitchen".to_string()));
-    hallway_exits.insert("e".to_string(), unend::Exit::Closed("It's not time for gardening.".to_string()));
     let hallway = unend::BasicSection::new(
         "kitchen".to_string(),
         "Main hallway".to_string(),
         "You are on the main hallway. You can go north to the kitchen, south to the sitting room, ...".to_string(),
-        hallway_exits
-        // exits: unend::ClosureExits {
-        //     n : || { println!("Going to the kitchen"); },
-        // },
+        hashmap!{
+            "n".to_string() => unend::Exit::Visitable("kitchen".to_string()),
+            "e".to_string() => unend::Exit::Closed("There's no time for gardening.".to_string()),
+        }
     );
 
-    let mut kitchen_exits = HashMap::new();
-    kitchen_exits.insert("s".to_string(), unend::Exit::Visitable("hallway".to_string()));
-    kitchen_exits.insert("e".to_string(), unend::Exit::Closed("You can't exit through the window.".to_string()));
     let kitchen = unend::BasicSection::new(
         "kitchen".to_string(),
         "The grand kitchen".to_string(),
         "You are at the center of the kitchen and dining room.".to_string(),
-        kitchen_exits
+        hashmap!{
+            "s".to_string() => unend::Exit::Visitable("hallway".to_string()),
+            "e".to_string() => unend::Exit::Closed("You can't exit through the window.".to_string()),
+        }
     );
 
-    let mut sections = HashMap::new();
-    sections.insert("hallway".to_string(), hallway);
-    sections.insert("kitchen".to_string(), kitchen);
+    let sections = hashmap!{
+        "hallway".into() => hallway,
+        "kitchen".into() => kitchen,
+    };
 
     let game = unend::Game::new(sections, "hallway");
 
